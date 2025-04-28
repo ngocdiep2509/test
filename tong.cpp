67356945136c56b6710c78f3c1b9d7dec257d0a3
ngocdiep2509 <diepchungoc259@gmail.com>
2025-04-28 20:34:24 +0700

    Update tong.cpp

--- tong.cpp
+++ tong.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     int a = 5, b = 3, c = 2;
-    int sum = a + b + c;
-    cout << "Tong: " << sum << endl;
+    int hieu = a - b - c;
+    cout << "Hieu: " << hieuhieu << endl;
     return 0;
 }
